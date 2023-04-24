c88d23eeae3bff89088947cdd4d1cb00a26b4ed9
Magdalena Pastuła <pastula.magdalena@gmail.com>
2023-04-24 15:14:27 +0200

    Added version of codec with no division
    Signed-off-by: Magdalena Pastuła <pastula.magdalena@gmail.com>

---
+++ streaming_ans/streaming_ans.srcs/sim_1/new/tb_streaming_ans_no_div.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.04.2023 14:19:57
+// Design Name: 
+// Module Name: tb_streaming_ans_beh
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_streaming_ans_no_div;
+    parameter STATE_WIDTH = 32;
+    
+    reg clk, reset, start;
+    reg [STATE_WIDTH-1:0] freq;
+    reg [7:0] symbol;
+
+	wire state_ready;
+	wire [STATE_WIDTH-1:0] state_out;
+	wire [STATE_WIDTH-3:0] bitstream;
+	wire [5:0] bistream_width;
+	
+	streaming_ans_no_div #(.state_width(STATE_WIDTH)) codec_no_div(clk, reset, start, freq, symbol, state_ready, state_out, bitstream, bistream_width);
+	                               
+	always
+	   #5 clk = ~clk;
+	   
+    initial
+        begin
+            clk = 0;
+            reset = 0;
+            start = 0;
+            freq = 0;
+            symbol = 0;
+            
+            #10; reset = 1;
+            #10; freq = 4; start = 1;
+            
+            /* Testing string: aabc ddaa cbab */
+            #10; freq = 5; /* a */
+            #10; freq = 3; /* b */
+            #10; freq = 2; /* c, d */
+            
+            #20; symbol = 0; /* a, a */
+            #20; symbol = 1; /* b */
+            #10; symbol = 2; /* c */
+            #10; symbol = 3; /* d, d */
+            #20; symbol = 0; /* a, a */
+            #20; symbol = 2; /* c */
+            #10; symbol = 1; /* b */
+            #10; symbol = 0; /* a */
+            #10; symbol = 1; /* b */
+            
+            
+            #30 $finish;
+            
+        end
+	                               
+endmodule
---
+++ streaming_ans/streaming_ans.srcs/sources_1/new/streaming_ans_no_div.v
@@ -0,0 +1,106 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.03.2023 11:15:34
+// Design Name: 
+// Module Name: streaming_ans_beh
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module streaming_ans_no_div
+    #(  parameter ans_state_width = 32)
+    (   input wire clk,
+        input wire reset,
+        input wire start,
+        input wire [ans_state_width-2:0] freq,
+        input wire [7:0] symbol,
+        output reg output_ready,
+        output reg [ans_state_width-1:0] ans_state,
+        output reg [ans_state_width-3:0] bitstream,
+        output reg [5:0] bitstream_width
+    );
+    
+    parameter [2:0] S0=3'd0,S1=3'd1,S2=3'd2,S3=3'd3; /*S4=3'd4,S5=3'd5,S6=3'd6;*/
+    reg [3:0] state;
+    reg [2:0] alphabet_size;
+    reg [7:0] freq_indx;
+    reg [ans_state_width-2:0] freqs [255:0];
+    reg [ans_state_width-2:0] M; 
+    reg [ans_state_width-2:0] C [255:0];
+    reg [ans_state_width-2:0] symbol_indx;
+    
+    wire [ans_state_width-2:0] M_next;
+    
+    integer i;
+    
+    always @(posedge clk) begin
+        if (!reset) begin
+            output_ready <= 0;
+            ans_state <= 0;
+            bitstream <= 0;
+            bitstream_width <= 0;
+            state <= S0;
+            alphabet_size <= 0;
+            freq_indx <= 0;
+            M <= 0;
+            symbol_indx <= 0;
+            for (i=0; i<256; i=i+1) begin
+                freqs[i] <= 0;
+                C[i] <= 0;
+            end
+        end
+        else begin
+            case (state)
+            S0: begin /* IDLE */
+               if (start == 1) begin
+                state <= S1;
+                alphabet_size <= freq[2:0];
+               end
+            end
+            S1: begin /* PREINIT - read frequencies for symbols and prepare for ANS */
+               freqs[freq_indx] <= freq;
+               freq_indx <= freq_indx + 1;
+               M <= M + freq;
+               C[freq_indx + 1] <= C[freq_indx] + freq;
+               if (freq_indx + 1 == alphabet_size) begin
+                state <= S2;
+                ans_state <= M + freq;
+               end
+            end
+            S2: begin /* STEP1 - count next state and bitstream */
+                bitstream <= ans_state[ans_state_width-3:0];
+                for (i=0; i<ans_state_width-2; i=i+1) begin
+                    if ((ans_state >> i) < 2 * freqs[symbol] && (ans_state >> i) >= freqs[symbol]) begin
+                        bitstream_width = i;
+                    end
+                end
+                ans_state <= M + C[symbol] + (ans_state >> bitstream_width) - freqs[symbol];
+                if (symbol_indx + 1 == M) begin
+                    output_ready <= 1;
+                    state <= S3;
+                end else begin
+                    symbol_indx <= symbol_indx + 1;
+                end
+            end
+            S3: begin /* END - hold state_out and bitstream values */
+                
+            end
+            endcase
+        end
+    end
+        
+    
+endmodule
